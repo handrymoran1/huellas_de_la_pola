96ab4d8c034109b3182494e70f79d144b9840d7f
fasalpa <psfr97@gmail.com>
2026-04-22 17:54:23 -0500

    Lógica de JS, agregar localStorage (aún por revisión del almacenamiento)

--- js/app.js
+++ js/app.js
@@ -2,24 +2,40 @@ const btnEnviarHabitacion = document.getElementById("btnEnviarHabitacion");
 const opcionesLista = document.getElementById("opcionesLista");
 const listaContenedor = document.getElementById("lista");
 
-// let listaHabitaciones = {habitación:};
-
 function agregarALista(nombreHabitacion) {
   listaContenedor.innerHTML = `<li class="bg-light p-2 border rounded d-inline-block">
               ${nombreHabitacion}
-              <a href="#">Info</a>
-              <button type="btnDisponibilidad">Disponible</button>
-              <button type="btnDisponibilidad">No disponible</button>
+              <a href="">Info</a>
+              <button type="button">Disponible</button>
+              <button type="button">No disponible</button>
+              <button type="button" id="btnGuardar">Publicar Habitación</button>
             </li>`;
+  console.log("Seleccionó: " + nombreHabitacion);
 
-  console.log("funcionó" + nombreHabitacion);
+  const btnNuevo = document.getElementById("btnGuardar");
+
+  btnNuevo.addEventListener("click", function () {
+    console.log("vamos bien");
+
+    const nuevaHabitacion = {
+      nombre: nombreHabitacion,
+      estado: "Pendiente",
+    };
+
+    const guardar = JSON.parse(localStorage.getItem("guardar")) || [];
+    guardar.push({ datosHabitacion });
+
+    localStorage.setItem("guardar", JSON.stringify(guardar));
+    console.log("Habitación agregada con éxito");
+  });
 }
 
 btnEnviarHabitacion.addEventListener("click", function () {
   console.log("El botón funciona.");
 
-  const habitacionSeleccionada = opcionesLista.options[opcionesLista.selectedIndex].text;
-  
+  const habitacionSeleccionada =
+    opcionesLista.options[opcionesLista.selectedIndex].text;
+
   if (opcionesLista.value.trim() === "") {
     alert("Seleccione una habitación.");
     return;
@@ -28,3 +44,19 @@ btnEnviarHabitacion.addEventListener("click", function () {
   }
   agregarALista(habitacionSeleccionada);
 });
+
+function mostrarLista() {
+  const guardadas = JSON.parse(localStorage.getItem("guardar")) || [];
+
+  guardadas.array.forEach((hab) => {
+    const nuevoElemento = document.createElement("li");
+    nuevoElemento.className = "bg-light p-2 border rounded mb-2";
+
+    nuevoElemento.innerHTML = `${hab.nombre} 
+    <span class="badge bg-primary">Publicada</span>`;
+
+listaContenedor.appendChild(nuevoElemento);
+
+  });
+}
+document.addEventListener("DOMContentLoaded", mostrarLista);
